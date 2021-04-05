e7508333a6c9553320ace6c48177b92f9159fdce
samwei12 <dongxiaosen@icloud.com>
2021-04-05 20:40:14 +0800

    优化冒泡排序

--- java/src/main/java/com/samwei12/sort/BubbleSort.java
+++ java/src/main/java/com/samwei12/sort/BubbleSort.java
@@ -13,12 +13,17 @@ public class BubbleSort {
             return;
         }
         for (int i = 0; i < arr.length - 1; i++) {
+            boolean swap = false;
             for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j+1]) {
+                if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swap = true;
                 }
+            }
+            if (swap) {
+                break;
             }
         }
     }
